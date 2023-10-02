6631920483464ef624f08567a0e0457512088438
MasterKong42 <139598525+MasterKong42@users.noreply.github.com>
2023-10-02 11:42:32 -0700

    coded coins

---
+++ Assets/Ansell Scripts/Health.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+
+//float timer;
+//public float originalTimer = 1.5;
+
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //timer = originalTimer;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //timer -= time.Deltatime;
+        //if (timer < 0) ;
+        {
+            
+            
+        }
+        
+    }
+}
