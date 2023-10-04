2d703a306e8f5e792b8be759cd5cc3372fcbcf9c
MasterKong42 <139598525+MasterKong42@users.noreply.github.com>
2023-10-04 12:02:18 -0700

    Coin, health, Iframe, health

--- Assets/Ansell Scripts/Health.cs
+++ Assets/Ansell Scripts/Health.cs
@@ -1,30 +1,71 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Health : MonoBehaviour
-
-//float timer;
-//public float originalTimer = 1.5;
-
+public class HealthCode : MonoBehaviour
 {
+    private float iframesTimer;
+    private float iframesTimerDefault = 1.5f;
+    private bool iframes = false;
+    private int health = 10;
+    
+    private Coin coinpurse;
     // Start is called before the first frame update
     void Start()
     {
-        //timer = originalTimer;
-        
+        iframesTimer = iframesTimerDefault;
+    }
+
+    private void Update()
+    {
+        if (iframes)
+        {
+            iframesTimer -= Time.deltaTime;
+            if (iframesTimer < 0)
+            {
+                iframes = false;
+                //reset the timer
+                iframesTimer = iframesTimerDefault;
+            }
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void OnCollisionEnter2D(Collision2D collision)
     {
-        //timer -= time.Deltatime;
-        //if (timer < 0) ;
+        if (collision.gameObject.CompareTag("Spikes"))
         {
-            
-            
+            if (!iframes)
+            {
+                ChangeHealth(-1);
+                iframes = true;
+            }
+
+            if (health < 1)
+            {
+                Death();
+            }
+
         }
+    }
+
+    void Death()
+    {
+        coinpurse.Coins = 0;
         
+        SceneManager.LoadScene("Start", LoadSceneMode.Single);
+        Debug.Log("You died :( . . . Your Coins  have been rest to ZERO! ");
+    }
+    
+    
+    
+    void ChangeHealth(int amount)
+    {
+        health += amount;
+        Debug.Log("Health: "+health);
+
     }
 }
