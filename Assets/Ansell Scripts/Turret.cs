7f4558cda5a914d0b3e21c2298e5481d4f0fec4a
MasterKong42 <139598525+MasterKong42@users.noreply.github.com>
2023-10-10 11:41:23 -0700

    got the fire balls to do some cray things

---
+++ Assets/Ansell Scripts/Turret.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Turret : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject Fireball ;
+    public float timer;
+ 
+    public float inttimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 1;
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"));
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < 0) ;
+        {
+            GameObject projectile = Instantiate(Fireball, transform.position, Quaternion.identity);
+            Vector3 target = Player.transform.position;
+            projectile.transform.Translate(target);
+            timer = inttimer;
+
+        }
+        
+    }
+
+   
+}
